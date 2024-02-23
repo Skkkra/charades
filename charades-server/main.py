fa8650596144070eebe622efa9d97ee8b68db198
Krystian Zbyrad <Krystian12053@gmail.com>
2024-02-23 23:52:56 +0100

    fix(core): updated api cors

--- charades-server/main.py
+++ charades-server/main.py
@@ -2,7 +2,7 @@ from flask import Flask
 from flask_cors import CORS
 
 app = Flask(__name__)
-CORS(app, resources={r"/api/*": {"origins": "*"}})
+CORS(app, resources={r"/*": {"origins": "*"}})
 
 
 @app.route('/', methods=['GET', 'POST'])
