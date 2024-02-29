7026a28d8a52d8012a7ce7dac31e2ad73f892ab8
Krystian Zbyrad <Krystian12053@gmail.com>
2024-02-29 11:50:54 +0100

    feat(core): Added websocket Created methods: connected, handle-message, disconnected used flask_socketio library

--- charades-server/main.py
+++ charades-server/main.py
@@ -1,8 +1,31 @@
-from flask import Flask
+from flask import Flask, request
 from flask_cors import CORS
+from flask_socketio import SocketIO, emit
 
 app = Flask(__name__)
+app.config['SECRET_KEY'] = 'secret!'
 CORS(app, resources={r"/*": {"origins": "*"}})
+socketio = SocketIO(app, cors_allowed_origins="*")
+
+
+# Websocket
+@socketio.on("connect")
+def connected():
+    print(request.sid)
+    print("client has connected")
+    emit("connect", {"data": f"id: {request.sid} is connected"})
+
+
+@socketio.on("data")
+def handle_message(data):
+    print("data from the frontend: ", str(data))
+    emit("data", {"data": data, "id": request.sid}, broadcast=True)
+
+
+@socketio.on("disconnect")
+def disconnected():
+    print("user disconnected")
+    emit("disconnect", f"user {request.sid} disconnected", broadcast=True)
 
 
 @app.route('/', methods=['GET', 'POST'])
@@ -11,4 +34,5 @@ def welcome():
 
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=105)
+    socketio.run(app, debug=True, port=5001, allow_unsafe_werkzeug=True, log_output=False, use_reloader=False)
+
