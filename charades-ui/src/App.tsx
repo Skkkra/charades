3a8301962abf6649f00700bf85c9c2d4dfc16be7
Krystian Zbyrad <Krystian12053@gmail.com>
2024-03-29 01:32:58 +0100

    feature(core): added websocket client created first connection

--- charades-ui/src/App.tsx
+++ charades-ui/src/App.tsx
@@ -1,24 +1,43 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import './App.scss';
+import { socket } from './socket';
 
 function App() {
-  const [count, setCount] = useState(0);
+  const [isConnected, setIsConnected] = useState(socket.connected);
+
+  useEffect(() => {
+    function onConnect() {
+      setIsConnected(true);
+    }
+
+    function onDisconnect() {
+      setIsConnected(false);
+    }
+
+    socket.on('connect', onConnect);
+    socket.on('disconnect', onDisconnect);
+
+    return () => {
+      socket.off('connect', onConnect);
+      socket.off('disconnect', onDisconnect);
+    };
+  }, []);
 
   return (
     <>
-      <div></div>
       <h1>Vite + React</h1>
       <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
+        <button
+          onClick={() => {
+            if (!isConnected) {
+              socket.connect();
+            }
+          }}
+        >
+          connect to WebScoket
         </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
+        <p>connected: {'' + isConnected}</p>
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
   );
 }
