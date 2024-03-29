cb4231ed32bfd0a1bf396915c248bf959328363b
Krystian Zbyrad <Krystian12053@gmail.com>
2024-03-29 01:31:37 +0100

    fixed(websocket) tweaked websocket server

--- charades-server/main.py
+++ charades-server/main.py
@@ -1,6 +1,6 @@
-from flask import Flask, request
+from flask import Flask
 from flask_cors import CORS
-from flask_socketio import SocketIO, emit
+from flask_socketio import SocketIO, emit, send, join_room, leave_room
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'secret!'
@@ -10,22 +10,37 @@ socketio = SocketIO(app, cors_allowed_origins="*")
 
 # Websocket
 @socketio.on("connect")
-def connected():
-    print(request.sid)
+def connected(socket):
     print("client has connected")
-    emit("connect", {"data": f"id: {request.sid} is connected"})
+    emit("connect", {"data": "id: User connected"})
 
 
-@socketio.on("data")
+@socketio.on("message")
 def handle_message(data):
     print("data from the frontend: ", str(data))
-    emit("data", {"data": data, "id": request.sid}, broadcast=True)
+    emit("data", {"data": data}, broadcast=True)
 
 
 @socketio.on("disconnect")
 def disconnected():
     print("user disconnected")
-    emit("disconnect", f"user {request.sid} disconnected", broadcast=True)
+    emit("disconnect", {"data": "User disconnected"}, broadcast=True)
+
+
+@socketio.on('join')
+def on_join(data):
+    username = data['username']
+    room = data['room']
+    join_room(room)
+    send(username + ' has entered the room.', to=room)
+
+
+@socketio.on('leave')
+def on_leave(data):
+    username = data['username']
+    room = data['room']
+    leave_room(room)
+    send(username + ' has left the room.', to=room)
 
 
 @app.route('/', methods=['GET', 'POST'])
@@ -35,4 +50,3 @@ def welcome():
 
 if __name__ == '__main__':
     socketio.run(app, debug=True, port=5001, allow_unsafe_werkzeug=True, log_output=False, use_reloader=False)
-
